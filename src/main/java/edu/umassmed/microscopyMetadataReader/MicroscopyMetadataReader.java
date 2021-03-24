2957366be775a4061f417e95c63186a4551ac040
akramhar <alex.83@bluewin.ch>
2021-03-24 19:05:11 -0400

    Modified boolean interpretation & mapping

--- src/main/java/edu/umassmed/microscopyMetadataReader/MicroscopyMetadataReader.java
+++ src/main/java/edu/umassmed/microscopyMetadataReader/MicroscopyMetadataReader.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -42,22 +44,22 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class MicroscopyMetadataReader {
-
+	
 	static boolean debug = false;
 	static boolean printFiles = true;
-	
-	static String settingsMapFileName = "image_settings_map.txt";
-	
-	static int xmlSpaces = 3;
 
+	static String settingsMapFileName = "image_settings_map.txt";
+
+	static int xmlSpaces = 3;
+	
 	public static Map<String, String> importSettingsMap(final String fileName)
 			throws IOException {
 		final Map<String, String> settingsMap = new LinkedHashMap<String, String>();
-		
+
 		final File file = new File(fileName);
 		final FileReader isr = new FileReader(file);
 		final BufferedReader br = new BufferedReader(isr);
-		
+
 		String line = br.readLine();
 		while (line != null) {
 			if (line.startsWith("//") || line.equals("")) {
@@ -67,21 +69,21 @@ public class MicroscopyMetadataReader {
 			final String[] values = line.split(":");
 			final String originalValue = values[0];
 			final String newValue = values[1];
-
-			settingsMap.put(originalValue, newValue);
 			
+			settingsMap.put(originalValue, newValue);
+
 			line = br.readLine();
 		}
-		
+
 		isr.close();
 		br.close();
 		return settingsMap;
 	}
-	
+
 	public static String getOMEXML(final IFormatReader reader)
 			throws MissingLibraryException, ServiceException {
 		final MetadataStore ms = reader.getMetadataStore();
-		
+
 		OMEXMLService service;
 		try {
 			final ServiceFactory factory = new ServiceFactory();
@@ -100,7 +102,7 @@ public class MicroscopyMetadataReader {
 			// .println("The metadata could not be converted to OME-XML.");
 			return null;
 	}
-	
+
 	public static String transformDate(final String attrDate) {
 		final String dates[] = attrDate.split("T");
 		final String date = dates[0];
@@ -113,7 +115,7 @@ public class MicroscopyMetadataReader {
 				+ " @ " + time;
 		return newDate;
 	}
-	
+
 	public static Map<String, List<String>> mergeMap(
 			final Map<String, List<String>> map1,
 			final Map<String, List<String>> map2) {
@@ -126,15 +128,15 @@ public class MicroscopyMetadataReader {
 				list2.addAll(list1);
 			}
 			mergedMap.put(key2, list2);
-			
+
 		}
 		return mergedMap;
 	}
-	
+
 	public static Map<String, List<String>> collectNodeFields(
 			final String path, final Node node) {
 		final Map<String, List<String>> valueMap = new LinkedHashMap<String, List<String>>();
-		
+
 		final NamedNodeMap attributes = node.getAttributes();
 		if (attributes == null)
 			return valueMap;
@@ -167,7 +169,7 @@ public class MicroscopyMetadataReader {
 		}
 		return valueMap;
 	}
-	
+
 	public static Map<String, List<String>> collectNodeListFields(
 			final String path, final NodeList childrenNodeList) {
 		Map<String, List<String>> valueMap = new LinkedHashMap<String, List<String>>();
@@ -183,14 +185,14 @@ public class MicroscopyMetadataReader {
 			} else {
 				newPath = nodeName;
 			}
-			
+
 			if (found.contains(newPath)) {
 				duplicates.add(newPath);
 			} else {
 				found.add(newPath);
 			}
 		}
-
+		
 		for (int i = 0; i < childrenNodeList.getLength(); i++) {
 			// final String tag = oldTag + "TAG" + i + "|";
 			final Node node = childrenNodeList.item(i);
@@ -222,7 +224,8 @@ public class MicroscopyMetadataReader {
 			String nodeValue = node.getTextContent();
 			if ((nodeValue != null) && !nodeValue.equals("")) {
 				if (nodeName.toLowerCase().contains("date")) {
-					nodeValue = MicroscopyMetadataReader.transformDate(nodeValue);
+					nodeValue = MicroscopyMetadataReader
+							.transformDate(nodeValue);
 				}
 				List<String> valueList = null;
 				if (valueMap.containsKey(newPath)) {
@@ -235,23 +238,25 @@ public class MicroscopyMetadataReader {
 			}
 			final Map<String, List<String>> nodeValueMap = MicroscopyMetadataReader
 					.collectNodeFields(newPath, node);
-			valueMap = MicroscopyMetadataReader.mergeMap(valueMap, nodeValueMap);
+			valueMap = MicroscopyMetadataReader
+					.mergeMap(valueMap, nodeValueMap);
 			final NodeList subChildren = node.getChildNodes();
 			if (subChildren.getLength() > 0) {
 				final Map<String, List<String>> subValueMap = MicroscopyMetadataReader
 						.collectNodeListFields(newPath, subChildren);
-				valueMap = MicroscopyMetadataReader.mergeMap(valueMap, subValueMap);
+				valueMap = MicroscopyMetadataReader.mergeMap(valueMap,
+						subValueMap);
 			}
 		}
-		
+
 		return valueMap;
 	}
-	
+
 	public static Map<String, List<String>> collectAllFields(final String xml)
 			throws ParserConfigurationException, SAXException, IOException {
-		
+
 		final InputSource is = new InputSource(new StringReader(xml));
-		
+
 		final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		// an instance of builder to parse the specified xml file
 		final DocumentBuilder db = dbf.newDocumentBuilder();
@@ -259,22 +264,22 @@ public class MicroscopyMetadataReader {
 		doc.getDocumentElement().normalize();
 		// System.out.println("Root element: "
 		// + doc.getDocumentElement().getNodeName());
-		
+
 		final NodeList rootChildren = doc.getDocumentElement().getChildNodes();
-		
+
 		final Map<String, List<String>> valueMap = MicroscopyMetadataReader
 				.collectNodeListFields(null, rootChildren);
-		
+
 		return valueMap;
 	}
-
+	
 	public static List<List<String>> normalizeString(final String s) {
 		final List<String> oldPartialKeys = new ArrayList<String>();
 		final List<String> oldKeyNames = new ArrayList<String>();
 		String normalized = s;
-
+		
 		while (normalized.contains("#")) {
-			
+
 			final String keySplit[] = normalized.split("#");
 			final int i1 = keySplit[0].lastIndexOf(".") + 1;
 			final int i2 = normalized.indexOf(".", i1 + 1);
@@ -284,29 +289,29 @@ public class MicroscopyMetadataReader {
 			} else {
 				oldPartialKey = normalized.substring(i1);
 			}
-			
+
 			final String oldKeySplit[] = oldPartialKey.split("#");
 			final String oldKeyName = oldKeySplit[0];
 			normalized = normalized.replace(oldPartialKey, oldKeyName);
-			
+
 			oldPartialKeys.add(oldPartialKey);
 			oldKeyNames.add(oldKeyName);
 		}
-
+		
 		final List<String> normalizedList = new ArrayList<String>();
 		normalizedList.add(normalized);
-
+		
 		final List<List<String>> values = new ArrayList<List<String>>();
 		values.add(normalizedList);
 		values.add(oldPartialKeys);
 		values.add(oldKeyNames);
 		return values;
 	}
-	
+
 	public static String createJSONString(
 			final Map<String, List<String>> valueMap,
 			final Map<String, String> settingsMap) throws JSONException {
-		
+
 		final List<String> mainObjs = new ArrayList<String>();
 		for (final String key : valueMap.keySet()) {
 			String newKey = null;
@@ -331,7 +336,7 @@ public class MicroscopyMetadataReader {
 			if (newKey == null) {
 				continue;
 			}
-			
+
 			final String newKeySplit[] = newKey.split("\\.");
 			final String attrKey = newKeySplit[newKeySplit.length - 1];
 			final String newKeyNoAttr = newKey.replace("." + attrKey, "");
@@ -346,7 +351,7 @@ public class MicroscopyMetadataReader {
 						&& mainKey.replace(newKeyNoAttr, "").contains(".")) {
 					notfound = false;
 				} else {
-					
+
 				}
 			}
 			if (notfound) {
@@ -357,7 +362,7 @@ public class MicroscopyMetadataReader {
 				mainObjs.add(replaceIndex, newKeyNoAttr);
 			}
 		}
-		
+
 		final JSONObject obj = new JSONObject();
 		for (final String objToCreate : mainObjs) {
 			final String pathToCreate[] = objToCreate.split("\\.");
@@ -396,11 +401,11 @@ public class MicroscopyMetadataReader {
 					}
 					currentObj = newObj;
 				}
-				
+
 			}
 		}
 		// System.out.println(obj.toString());
-
+		
 		for (final String key : valueMap.keySet()) {
 			if (key == null) {
 				if (MicroscopyMetadataReader.debug) {
@@ -434,14 +439,18 @@ public class MicroscopyMetadataReader {
 				}
 				continue;
 			}
-			
+
 			JSONObject currentObj = obj;
 			final String path[] = newKey.split("\\.");
 			for (int i = 0; i < (path.length); i++) {
 				final String pToAttr = path[i];
 				if (i == (path.length - 1)) {
 					Object newValue = value;
-					if (Boolean.parseBoolean(value)) {
+					final Pattern pattern = Pattern.compile("true|false",
+							Pattern.CASE_INSENSITIVE);
+					final Matcher matcher = pattern.matcher(value);
+					if (matcher.matches()) {
+						// if (Boolean.parseBoolean(value)) {
 						if (MicroscopyMetadataReader.debug) {
 							System.out.println(newValue + " is boolean");
 						}
@@ -456,7 +465,7 @@ public class MicroscopyMetadataReader {
 							System.out.println(newValue + " is string");
 						}
 					}
-
+					
 					currentObj.put(pToAttr, newValue);
 					continue;
 				}
@@ -486,7 +495,7 @@ public class MicroscopyMetadataReader {
 				}
 			}
 		}
-		
+
 		// for (final String oldKey : valueMap.keySet()) {
 		// String newKey = settingsMap.get(oldKey);
 		// if (newKey == null) {
@@ -502,7 +511,7 @@ public class MicroscopyMetadataReader {
 		// System.out.println(obj.toString());
 		return obj.toString();
 	}
-
+	
 	public static boolean isNumeric(final String str) {
 		try {
 			Double.parseDouble(str);
@@ -511,7 +520,7 @@ public class MicroscopyMetadataReader {
 			return false;
 		}
 	}
-	
+
 	public static void main(final String[] args) {
 		// parse command line arguments
 		try {
@@ -532,7 +541,7 @@ public class MicroscopyMetadataReader {
 			int series = args.length > 1 ? Integer.parseInt(args[1]) : 0;
 			final PrintStream origOut = System.out;
 			System.setOut(null);
-			
+
 			// create OME-XML metadata store
 			final ServiceFactory factory = new ServiceFactory();
 			final OMEXMLService service = factory
@@ -543,13 +552,13 @@ public class MicroscopyMetadataReader {
 			reader.setMetadataStore(meta);
 			// initialize file
 			reader.setId(id);
-			
+
 			final int seriesCount = reader.getSeriesCount();
 			if (series < seriesCount) {
 				reader.setSeries(series);
 			}
 			series = reader.getSeries();
-			
+
 			final File f = new File(System.getProperty("user.dir"));
 			final File dir = f.getAbsoluteFile();
 			String directoryPath = dir.toString();
@@ -563,12 +572,12 @@ public class MicroscopyMetadataReader {
 				settingsFileName = directoryPath + File.separator
 						+ MicroscopyMetadataReader.settingsMapFileName;
 			}
-			
+
 			final Map<String, String> settingsMap = MicroscopyMetadataReader
 					.importSettingsMap(settingsFileName);
-			
+
 			final String xml = MicroscopyMetadataReader.getOMEXML(reader);
-			
+
 			final String indentXML = XMLTools.indentXML(xml,
 					MicroscopyMetadataReader.xmlSpaces, true);
 			if (MicroscopyMetadataReader.printFiles) {
@@ -580,7 +589,7 @@ public class MicroscopyMetadataReader {
 				fw.close();
 			}
 			// System.out.println(settingsMap);
-			
+
 			final Map<String, List<String>> valueMap = MicroscopyMetadataReader
 					.collectAllFields(xml);
 			// fileName = id + ".json";
@@ -595,13 +604,13 @@ public class MicroscopyMetadataReader {
 			// }
 			// System.out.println(s + " - " + valueMap.get(s));
 			// }
-			
+
 			if (MicroscopyMetadataReader.debug) {
 				System.setOut(origOut);
 			}
-			
-			final String jsonString = MicroscopyMetadataReader.createJSONString(
-					valueMap, settingsMap);
+
+			final String jsonString = MicroscopyMetadataReader
+					.createJSONString(valueMap, settingsMap);
 			if (MicroscopyMetadataReader.printFiles) {
 				final String fileName = id + ".json";
 				final FileWriter fw = new FileWriter(fileName);
